8c11a37691ef8883597ba9d2f87857af2c18aa02
Sameet <sameetghouri@yahoo.com>
2023-03-02 00:23:14 +0500

    forks added

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+const About = () => {
+    return ( <div>
+        <h1>About</h1>
+        <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+    </div> );
+}
+ 
+export default About;
--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main >
-      <h1>Homepage</h1>
+     <h1>HomePage</h1>
     </main>
   )
 }
