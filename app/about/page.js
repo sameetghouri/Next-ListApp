e43e6aa828bcb5802c240b3cbff41503c6076ed4
Sameet <sameetghouri@yahoo.com>
2023-03-04 23:57:40 +0500

    modified

--- app/about/page.js
+++ app/about/page.js
@@ -2,6 +2,7 @@ const About = () => {
     return ( <div>
         <h1>About</h1>
         <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
+        <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
     </div> );
 }
  
