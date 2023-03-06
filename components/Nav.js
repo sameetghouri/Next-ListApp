225be88c13d5acc84c1ec2e1f65b355eb9d8a428
Sameet <sameetghouri@yahoo.com>
2023-03-06 23:56:16 +0500

    modified

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,11 @@
 const Nav = () => {
     return ( <nav>
-        
+        <div>
+            <h1>Fork List</h1>
+        </div>
+        <a>Home</a>
+        <a>About</a>
+        <a>Fork Listing</a>
     </nav> );
 }
  
