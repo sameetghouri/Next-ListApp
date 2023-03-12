33579ee2c1a9f738b659444969b405c2675af1d4
Sameet <sameetghouri@yahoo.com>
2023-03-12 23:47:36 +0500

    modified

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import './globals.css'
+import Nav from '../components/Nav'
+import Footer from '@/components/Footer'
 
 export const metadata = {
   title: 'Create Next App',
@@ -8,7 +10,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Nav />
+        {children}
+        <Footer />
+
+      </body>
     </html>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
-import Nav from '../components/Nav'
-import Footer from '@/components/Footer'
 import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -10,13 +8,12 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main >
-      <Nav/>
+      
      <h1>HomePage</h1>
      <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.</p>
         
      <Link href='/forks'>See Forks Listing</Link>
     
-     <Footer/>
     </main>
   )
 }
