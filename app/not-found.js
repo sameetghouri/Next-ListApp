dd3268f22a7a1feb0a86c474687a015d4e7212b0
Sameet <sameetghouri@yahoo.com>
2023-03-17 00:05:44 +0500

    modified

---
+++ app/not-found.js
@@ -0,0 +1,20 @@
+"use client"
+import Link from "next/link";
+import { useRouter } from 'next/navigation';
+import { useEffect } from "react";
+
+const notFound = () => {
+    const router = useRouter();
+    useEffect(() => {
+        setTimeout(() => {
+            router.push("/");
+        }, 3000);
+    }, []);
+    return ( <div className="h-screen flex flex-col items-center justify-center">
+        <h1 className="text-3xl">404</h1>
+        <p className="text-2xl">Page not found</p>
+        <Link className="hover:text-sky-600" href='/'>Go Back To Home</Link>
+    </div> );
+}
+ 
+export default notFound;
--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,7 @@
 import Link  from 'next/link';
 const Nav = () => {
     return (
-    <nav className='my-4 mx-8 p-4 flex gap-3 justify-end align-bottom border-b-2'>
+    <nav className='w-full my-4 mx-8 p-4 flex gap-3 justify-end align-bottom border-b-2'>
         <div className='mr-auto font-bold text-xl'>
         <h1>Fork List</h1>
         </div>
