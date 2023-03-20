7a3c03cf720963c2f21494baf73c0953c5cdf120
Sameet <sameetghouri@yahoo.com>
2023-03-20 23:57:55 +0500

    modified

--- app/not-found.js
+++ app/not-found.js
@@ -11,7 +11,9 @@ const notFound = () => {
         }, 3000);
     }, []);
     return ( <div className="h-screen w-full flex flex-col items-center justify-center">
-      
+        <h1 className="text-3xl">404</h1>
+        <p className="text-2xl">Page not found</p>
+        <Link className="hover:text-sky-600" href='/'>Go Back To Home</Link>
     </div> );
 }
  
