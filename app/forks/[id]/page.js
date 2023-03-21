59e0c8d744b6fa979f01fad6fdc604a86239e35a
Sameet <sameetghouri@yahoo.com>
2023-03-22 00:38:53 +0500

    modified

--- app/forks/[id]/page.js
+++ app/forks/[id]/page.js
@@ -1,6 +1,24 @@
-const Details = () => {
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const fdata = await res.json();
+    const paths = fdata.map(item => {
+        return {
+            params: {id: item.id.toString()}
+        }
+    })
+    return {
+        paths,
+        fallback: false
+    }
+}
+const Details = async (contex) => {
+    const id = contex.params.id;
+    const res = await fetch('https://jsonplaceholder.typicode.com/users'+id);
+    const udata = await res.json();
     return ( <div>
-        <h1>Details</h1>
+        <h1>{udata.name}</h1>
+        <p>{udata.email}</p>
+        <p>{udata.website}</p>
     </div> );
 }
  
