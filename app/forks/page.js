fb25543e1549d2dde9cb7df9a22195a9ae38cf0e
Sameet <sameetghouri@yahoo.com>
2023-03-21 18:22:36 +0500

    modified

--- app/forks/page.js
+++ app/forks/page.js
@@ -1,15 +1,16 @@
 import Link from "next/link";
 
-async function getdata() {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await res.json();
-    return data 
-}
+// async function getdata() {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//     const data = await res.json();
+//     return data 
+// }
 
 const  forks = async () => {
-    const fdata = await getdata();
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const fdata = await res.json();
     
-    return ( <div>
+    return ( <div >
         <h1 className='text-2xl pb-4'>Forks</h1>
         {fdata.map(item =>(
             <div className=" p-2 m-2 rounded bg-slate-200 border-l-8 hover:border-l-cyan-700 " key={item.id}>
--- components/Nav.js
+++ components/Nav.js
@@ -2,7 +2,7 @@ import Link  from 'next/link';
 import Image from 'next/image';
 const Nav = () => {
     return (
-    <nav className='w-full p-4 flex gap-3 justify-end items-end border-b-2'>
+    <nav className='w-full mb-4 p-4 flex gap-3 justify-end items-end border-b-2'>
         <div className='mr-auto '>
         <Image src="/logo.png" alt='logo' width={50} height={50} />
         </div>
