09cb3f1a4b4224ea784ded3b3bde5de194886461
Sameet <sameetghouri@yahoo.com>
2023-03-23 22:05:09 +0500

    modified

--- app/forks/[id]/page.js
+++ app/forks/[id]/page.js
@@ -18,7 +18,13 @@ const Details = async(props) => {
           const udata = await res.json();
     
     return ( 
-        
+         <div className="flex flex-col gap-4 items-center ">
+        <div className="bg-gray-400 rounded py-20 px-12 hover:scale-105 transition duration-200">
+        <h1 className="text-xl font-semibold">{udata?.name }</h1>
+        <p className="text-lg">{`Email: ${udata?.email}` }</p>
+        <p className="text-lg">{`Website: ${udata?.website}` }</p>
+        <p className="text-lg">{`City: ${udata?.address.city}` }</p>
+        </div>
     </div> );
 }
  
