a657f3873c7ac4538fdeac9574cbbf508570fe43
Sameet <sameetghouri@yahoo.com>
2023-03-24 23:57:40 +0500

    modified

--- components/Nav.js
+++ components/Nav.js
@@ -1,14 +1,18 @@
+"use client";
 import Link  from 'next/link';
 import Image from 'next/image';
+import {motion} from 'framer-motion';
+import { usePathname } from 'next/navigation';
 const Nav = () => {
+    const path = usePathname();
     return (
     <nav className='w-full mb-4 p-3 flex gap-4 justify-end items-end border-b-2'>
         <div className='mr-auto'>
         <Image src="/logo.png" alt='logo' width={60} height={60} />
         </div>
-        <Link href="/" className='hover:text-sky-600 font-semibold'>Home</Link>
-        <Link href="/about" className='hover:text-sky-600 font-semibold' >About</Link>
-        <Link href="/forks" className='hover:text-sky-600 font-semibold'>Fork Listing</Link>
+        <Link href="/" className={`${path==="/" ? "text-sky-600 font-semibold" : "hover:text-sky-600 font-semibold"}`}>Home</Link>
+        <Link href="/about" className={`${path==="/about" ? "text-sky-600 font-semibold" : "hover:text-sky-600 font-semibold"}`} >About</Link>
+        <Link href="/forks" className={`${path==="/forks" ? "text-sky-600 font-semibold" : "hover:text-sky-600 font-semibold"}`}>Fork Listing</Link>
 
     </nav>);
 }
